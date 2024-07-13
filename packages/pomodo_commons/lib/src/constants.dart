677c6ac1a472a2743e854ea532be2c1bce764e70
G. Schubert <schubertyi@gmail.com>
2024-07-13 11:33:59 +0100

    feat(commons): updated contants

--- packages/pomodo_commons/lib/src/constants.dart
+++ packages/pomodo_commons/lib/src/constants.dart
@@ -1,7 +1,10 @@
+const kCommonsPackageName = 'pomodo_commons';
+
 abstract class Constants {
   static const pomodoTodoistProjectName = 'Pomodo';
 
-  static const todoistBaseUrl = 'https://api.todoist.com/rest/v2';
+  static const todoistBaseRestUrl = 'https://api.todoist.com/rest/v2';
+  static const todoistBaseSyncUrl = 'https://api.todoist.com/sync/v9';
   static const todoistTestToken = String.fromEnvironment('TODOIST_API_TEST_TOKEN');
 
   static const taskTrackingMetadataTag = 'pomodo_app:time_s';
