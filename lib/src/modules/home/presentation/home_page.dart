a41575736a013929506546540f317631409a4d28
G. Schubert <schubertyi@gmail.com>
2024-07-13 11:42:45 +0100

    feat(home): added kanban view to home and implemented base for filter

--- lib/src/modules/home/presentation/home_page.dart
+++ lib/src/modules/home/presentation/home_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:pomodo_commons/pomodo_commons.dart';
 
-import '../../project/presentation/components/kanban_view/kanban_view.dart';
-import '../../project/presentation/cubit/project_cubit.dart';
+import '../../../shared/models/project_model.dart';
+import '../../kanban/domain/objects/kanban_view_data.dart';
+import '../../kanban/presentation/views/kanban_view/cubit/kanban_cubit.dart';
+import '../../kanban/presentation/views/kanban_view/kanban_view.dart';
+import '../../project/presentation/cubit/project_store_cubit.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,55 +27,130 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 1,
         centerTitle: true,
-        shadowColor: Colors.black,
-        title: const Text('Pomodo'),
+        title: Text(i18n.appTitle),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          BlocBuilder<ProjectStoreCubit, ProjectStoreState>(
-            builder: (context, state) {
-              if (state is! ProjectStoreLoaded) {
-                return const SizedBox.shrink();
-              }
-
-              return const Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Search will go here'),
-                  ],
-                ),
-              );
-            },
-          ),
-          Expanded(
-            child: BlocBuilder<ProjectStoreCubit, ProjectStoreState>(
+      body: UnfocusInputField(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Gap(16),
+            BlocBuilder<ProjectStoreCubit, ProjectStoreState>(
               builder: (context, state) {
-                if (state is ProjectStoreError) {
-                  return Center(
-                    child: Text(state.message),
-                  );
-                }
-
                 if (state is! ProjectStoreLoaded) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
+                  return const SizedBox.shrink();
                 }
 
-                return KanbanView(project: state.project);
+                return const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: _KanbanSearchRow(),
+                );
               },
             ),
-            // child: KanbanView(),
-          )
-        ],
+            Expanded(
+              child: BlocBuilder<ProjectStoreCubit, ProjectStoreState>(
+                builder: (context, state) {
+                  if (state is ProjectStoreError) {
+                    return Center(
+                      child: Text(state.message),
+                    );
+                  }
+
+                  if (state is! ProjectStoreLoaded) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  final columnsData = parseKanbanData(state.project);
+
+                  return BlocProvider<KanbanCubit>(
+                    create: (context) => AppKanbanCubit(
+                      columnsData: columnsData,
+                      taskRepository: context.read(),
+                    ),
+                    child: KanbanView(project: state.project),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  List<KanbanColumnViewData> parseKanbanData(Project project) {
+    final columnsData = <KanbanColumnViewData>[];
+
+    for (final section in project.sections) {
+      columnsData.add(
+        KanbanColumnViewData(
+          section: section,
+          tasks: project.tasks.where((element) => element.sectionId == section.id).toList()
+            ..sort((a, b) => a.order.compareTo(b.order)),
+        ),
+      );
+    }
+
+    return columnsData;
+  }
+}
+
+// TODO: Connect filter to states
+class _KanbanSearchRow extends StatelessWidget {
+  const _KanbanSearchRow();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          child: TextFormField(
+            decoration: InputDecoration(
+              filled: true,
+              hintText: i18n.home.label.searchTaskHint,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+              fillColor: AppColors.lightGrey,
+              prefixIcon: const AssetWidget(
+                Assets.searchIcon,
+                color: AppColors.grey,
+                fit: BoxFit.scaleDown,
+              ),
+              hintStyle: const TextStyle(color: AppColors.grey),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(color: AppColors.outlineGrey),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(color: AppColors.grey),
+              ),
+            ),
+          ),
+        ),
+        const Gap(8),
+        IconButton(
+          constraints: BoxConstraints.tight(const Size.square(48)),
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            backgroundColor: AppColors.lightGrey,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            side: const BorderSide(
+              color: AppColors.outlineGrey,
+            ),
+          ),
+          icon: const AssetWidget(
+            Assets.filterIcon,
+            color: AppColors.grey,
+          ),
+        )
+        // IconButton.outlined(onPressed: () {}, icon: const Icon(Icons.filter)),
+      ],
     );
   }
 }
