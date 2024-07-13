2c781eb6e194f8fd3fb562c706275897dc027f87
G. Schubert <schubertyi@gmail.com>
2024-07-13 11:34:12 +0100

    feat(commons): updated lib file from commons package

--- packages/pomodo_commons/lib/pomodo_commons.dart
+++ packages/pomodo_commons/lib/pomodo_commons.dart
@@ -3,13 +3,16 @@ library pomodo_commons;
 export 'package:dio/dio.dart' show CancelToken;
 export 'package:go_router/go_router.dart';
 
+export 'gen/strings.g.dart'
+    show BaseTranslations, i18n, TranslationProvider, AppLocaleUtils, LocaleSettings, Translations;
 export 'src/assets.dart';
 export 'src/client/client.dart' hide ClientResponse;
 export 'src/components/components.dart';
-export 'src/constants.dart';
+export 'src/constants.dart' hide kCommonsPackageName;
 export 'src/either.dart';
 export 'src/errors/errors.dart';
 export 'src/logger.dart';
 export 'src/module/app_module.dart';
 export 'src/style/style.dart';
 export 'src/types.dart';
+export 'src/validators/input_validators.dart';
