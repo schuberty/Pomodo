20966fdcbb5714a4ea89d49fd4fc8a66bf9d13c3
G. Schubert <schubertyi@gmail.com>
2024-07-13 11:42:02 +0100

    chore: centralized failures in one file

--- lib/src/core/errors/failures.dart
+++ lib/src/core/errors/failures.dart
@@ -9,3 +9,18 @@ class ObligatoryProjectSectionsNotCreated implements ClientError {
   @override
   final message = 'Obligatory Project sections not created';
 }
+
+class CommentNotDeleted implements ClientError {
+  @override
+  final message = 'Task comment not deleted';
+}
+
+class TaskNotDeleted implements ClientError {
+  @override
+  final message = 'Task not deleted';
+}
+
+class TaskCommandNotSynced implements ClientError {
+  @override
+  final message = 'At lest one Task command returned an error';
+}
